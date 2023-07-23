1bc458c060239927bed822d1caa3e6ceaeff575e
Asato65 <a.3ka319@gmail.com>
2023-07-24 08:35:16 +0900

    1

--- test.py
+++ test.py
@@ -9,8 +9,27 @@ rd.set_read_plan([1], "GEN2", bank=["epc"])
 # 例外のキャッチ，エラー表示
 rd.enable_exception_handler(lambda e: print(e))
 
-read_time = int(input("何秒間実行しますか？（単位：s）>>"))
+read_time = int(input("何秒間実行しますか？ >> "))
 
 rd.start_reading(lambda tag: print(tag.epc))
 time.sleep(read_time)
 rd.stop_reading()
+
+
+"""
+def func1(func):
+	func()
+
+def greeting():
+	print("hello")
+
+func1(greeting)
+
+
+def func1(func):
+	func()
+
+func1(lambda: print("hello"))
+
+
+"""
